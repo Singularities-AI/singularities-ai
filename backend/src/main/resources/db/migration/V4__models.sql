9ac13b5e2fb86b5d6fb088b152a61301706c11e4
BriacDelaigue <briac.delaigue@gmail.com>
2025-10-02 12:01:18 +0200

    feat(model): add Ollama models to database

---
+++ backend/src/main/resources/db/migration/V4__models.sql
@@ -0,0 +1,43 @@
+INSERT INTO models ("name", code, is_default, is_download, is_downloading, description)
+VALUES ('gpt-oss:20b', 'gpt-oss:20b', false, false, false,
+        'OpenAI’s open-weight models designed for powerful reasoning, agentic tasks, and versatile developer use cases.');
+
+INSERT INTO models ("name", code, is_default, is_download, is_downloading, description)
+VALUES ('gpt-oss:120b', 'gpt-oss:120b', false, false, false,
+        'OpenAI’s open-weight models designed for powerful reasoning, agentic tasks, and versatile developer use cases.');
+
+INSERT INTO models ("name", code, is_default, is_download, is_downloading, description)
+VALUES ('deepseek-r1:1.5b', 'deepseek-r1:1.5b', false, false, false,
+        'DeepSeek-R1 is a family of open reasoning models with performance approaching that of leading models, such as O3 and Gemini 2.5 Pro.');
+
+INSERT INTO models ("name", code, is_default, is_download, is_downloading, description)
+VALUES ('deepseek-r1:7b', 'deepseek-r1:7b', false, false, false,
+        'DeepSeek-R1 is a family of open reasoning models with performance approaching that of leading models, such as O3 and Gemini 2.5 Pro.');
+
+INSERT INTO models ("name", code, is_default, is_download, is_downloading, description)
+VALUES ('deepseek-r1:8b', 'deepseek-r1:8b', false, false, false,
+        'DeepSeek-R1 is a family of open reasoning models with performance approaching that of leading models, such as O3 and Gemini 2.5 Pro.');
+
+INSERT INTO models ("name", code, is_default, is_download, is_downloading, description)
+VALUES ('deepseek-r1:14b', 'deepseek-r1:14b', false, false, false,
+        'DeepSeek-R1 is a family of open reasoning models with performance approaching that of leading models, such as O3 and Gemini 2.5 Pro.');
+
+INSERT INTO models ("name", code, is_default, is_download, is_downloading, description)
+VALUES ('deepseek-r1:32b', 'deepseek-r1:32b', false, false, false,
+        'DeepSeek-R1 is a family of open reasoning models with performance approaching that of leading models, such as O3 and Gemini 2.5 Pro.');
+
+INSERT INTO models ("name", code, is_default, is_download, is_downloading, description)
+VALUES ('deepseek-r1:70b', 'deepseek-r1:70b', false, false, false,
+        'DeepSeek-R1 is a family of open reasoning models with performance approaching that of leading models, such as O3 and Gemini 2.5 Pro.');
+
+INSERT INTO models ("name", code, is_default, is_download, is_downloading, description)
+VALUES ('llama3.1:8b', 'llama3.1:8b', false, false, false,
+        'Llama 3.1 is a new state-of-the-art model from Meta available in 8B, 70B and 405B parameter sizes.');
+
+INSERT INTO models ("name", code, is_default, is_download, is_downloading, description)
+VALUES ('llama3.1:70b', 'llama3.1:70b', false, false, false,
+        'Llama 3.1 is a new state-of-the-art model from Meta available in 8B, 70B and 405B parameter sizes.');
+
+INSERT INTO models ("name", code, is_default, is_download, is_downloading, description)
+VALUES ('mistral:7b', 'mistral:7b', false, false, false,
+        'The 7B model released by Mistral AI, updated to version 0.3.');
